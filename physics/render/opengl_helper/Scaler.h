820c5913efd315e7b3769b549444d9b96f26160a
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-04-20 07:05:57 +0800

    add: smoothing for scale to reduce jitter

--- physics/render/opengl_helper/Scaler.h
+++ physics/render/opengl_helper/Scaler.h
@@ -2,14 +2,27 @@
 #define SCALER_H_INCLUDED
 
 #include "../../math/Vector.h"
+#include <iostream>
 
+#define BIG_NUMBER 1e12
 namespace GLPhysX {
-	inline PMath::Vector  Scale(PMath::Vector x, int width, int height) {
+
+	inline int Discretize(float x) {
+		return (x * BIG_NUMBER) / BIG_NUMBER;
+	}
+
+	inline PMath::Vector  Scale(PMath::Vector v, int width, int height) {
 		width /= 2;
 		height /= 2;
 
+		// Discretize a bit so smaller perturbations are unlikely 
+		float x = 1.0 * Discretize(v.vec[0]) / width;
+		float y = -1.0 * Discretize(v.vec[1]) / height;
+		float z = 1.0 * Discretize(v.vec[2]);
+		float w = 1.0 * Discretize(v.vec[3]);
+
 		// Need the - on the y coordinate so down points down.
-		return PMath::init(x.vec[0] / width, -x.vec[1] / height, x.vec[2], x.vec[3]);
+		return PMath::init(x, y, z, w);
 	}
 }
 
