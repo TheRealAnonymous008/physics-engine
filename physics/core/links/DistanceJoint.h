ec49572da57b601ecc0564d97e556ce3418786e1
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-04-21 05:18:30 +0800

    fix: lack of conservation in velocity

--- physics/core/links/DistanceJoint.h
+++ physics/core/links/DistanceJoint.h
@@ -22,18 +22,18 @@ namespace Physics {
 			const PMath::Vector midpoint = PMath::midpoint(first->transform.position, second->transform.position);
 			const PMath::Vector dir = PMath::normalize(first->transform.position - second->transform.position);
 
-			if (first->GetType() != BodyType::STATIC) {
+			if (first->GetType() != BodyType::STATIC && delta != 0) {
 				first->old_transform.position = first->transform.position;
 				first->transform.position = midpoint + dir * half_norm;
 				first->old_transform.velocity = first->transform.velocity;
-				first->transform.velocity += first->transform.position - first->old_transform.position;
+				first->transform.velocity += (first->transform.position - first->old_transform.position) / delta;
 			}
 
-			if (second->GetType() != BodyType::STATIC) {
+			if (second->GetType() != BodyType::STATIC && delta != 0) {
 				second->old_transform.position = second->transform.position;
 				second->transform.position = midpoint - dir * half_norm;
 				second->old_transform.velocity = second->transform.velocity;
-				second->transform.velocity += second->transform.position - second->old_transform.position;
+				second->transform.velocity += (second->transform.position - second->old_transform.position) / delta;
 			}
 		}
 	};
