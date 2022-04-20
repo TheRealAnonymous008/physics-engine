8626e20050397463ef3a69e1ecc8768f349d5f84
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-04-20 08:44:02 +0800

    add: Rigid shapes

--- physics/core/geometry/Triangle.h
+++ physics/core/geometry/Triangle.h
@@ -1,25 +1,29 @@
 #ifndef TRIANGLE_H_INCLUDED
 #define TRIANGLE_H_INCLUDED
 
+#include "Shape.h"
 #include "../points/Point.h"
+#include "../links/DistanceJoint.h"
 
 namespace Physics {
 	namespace Geometry {
-		class Triangle {
-		private:
-			Point* p1; 
-			Point* p2;
-			Point* p3;
+		// A shape with 3 points as vertices
+		class Triangle : public Shape{
 		public:
 			Triangle(Point* p1, Point* p2, Point* p3) {
-				this->p1 = p1;
-				this->p2 = p2;
-				this->p3 = p3;
+				this->points.push_back(p1);
+				this->points.push_back(p2);
+				this->points.push_back(p3);
 			}
-
-			Point** GetPoints() {
-				Point* points[3] = { p1, p2, p3 };
-				return points;
+		};
+		
+		// A triangle with fixed side lengths
+		class RigidTriangle : public Triangle {
+		public:
+			RigidTriangle(Point* p1, Point* p2, Point* p3) : Triangle(p1, p2, p3){
+				this->constraints.push_back(new DistanceJoint(p1, p2));
+				this->constraints.push_back(new DistanceJoint(p2, p3));
+				this->constraints.push_back(new DistanceJoint(p1, p3));
 			}
 		};
 	}
